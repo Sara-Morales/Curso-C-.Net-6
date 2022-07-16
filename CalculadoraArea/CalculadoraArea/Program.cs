b96a7236e1d21e9012980bdc204b14c3a2cd80ff
Sara-Morales <65975505+Sara-Morales@users.noreply.github.com>
2022-07-16 16:43:21 -0500

    CalculadoraArea
    Ejercicio de calculadora para un area de un circulo

---
+++ CalculadoraArea/CalculadoraArea/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+const double PI = 3.1416;
+string userData;
+double radius;
+double circleArea;
+
+Console.WriteLine("Ingresa el radio del circulo: ");
+userData = Console.ReadLine();
+
+if (double.TryParse(userData, out radius))
+{
+    circleArea = PI * Math.Pow(radius, 2);
+    Console.WriteLine($"El área de tu circulo es: {circleArea}");
+}
+else
+{
+    Console.WriteLine("Los datos proporcionados son incorrectos");
+}
+Console.Read();

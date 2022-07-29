8fcb02cff7d943b9fa3872857d5a756802ae3ea3
Sara-Morales <65975505+Sara-Morales@users.noreply.github.com>
2022-07-28 21:56:36 -0500

    EceptionError
    Uso de Try Catch

---
+++ TryCatchPersonality/TryCatchPersonality/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor introduce un número para dividir 100: ");
+            Divide(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+        static void Divide(string stringNum)
+        {
+            try
+            {
+                int num = int.Parse(stringNum);
+                int result = 100 / num;
+                Console.WriteLine($"100 / {num} = {result}");
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Se ha generado una excepción del tipo {ex.GetType().Name}");
+            }
+        }
+    }
+}

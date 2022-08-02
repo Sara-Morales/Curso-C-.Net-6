bbcb5c97a0a145858a079f7ee109b04b5ad75005
Sara-Morales <65975505+Sara-Morales@users.noreply.github.com>
2022-08-01 20:34:16 -0500

    Objetos

---
+++ POO/POO/Dog.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Dog
+{
+    //Estado u propiedades de la clase
+    public string Name { get; set; }
+    public string Bread { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+
+    //Comportamientos, funcionalidad
+    public void Bark ()
+    {
+        Console.WriteLine(Name + ": Guu, gua, guaa!");
+    }
+    public void Sleep()
+    {
+        Console.WriteLine(Name + ": Zzzzzzzzzzzzzz");
+    }
+    public void Eat()
+    {
+        Console.WriteLine(Name + ": Jaam, jam!");
+    }
+    public override string ToString()
+    {
+        return this.Name;
+    }
+
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Dog roky = new Dog();
+roky.Name = "Roky";
+roky.Bread = "Chihuahua";
+roky.Age = 3;
+roky.Color = "cafe";
+roky.Bark();
+roky.Eat();
+roky.Sleep();
+
+Dog dogo = new Dog();
+dogo.Name = "Dogo";
+dogo.Bread = "Pastor Alemán";
+dogo.Bark();
+dogo.Eat();
+dogo.Sleep();
+
+Console.WriteLine(dogo.ToString());
+Console.ReadKey();
